4359a46636135c76adfd00e555c5013335a749f0
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2026-09-11 07:05:57 -0300

    feat: create header

---
+++ src/components/Footer.svelte
@@ -0,0 +1,20 @@
+<script>
+  const date = new Date()
+  const currentYear = date.getFullYear()
+</script>
+
+<footer class="app-footer">
+  <span>
+    Pollstorm {currentYear} | All rights reserved
+  </span>
+</footer>
+
+<style>
+  .app-footer{
+    font-size: 1.1rem;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>

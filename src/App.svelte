7c46b6f3f7981f629b5d63cc288aa426f9080da2
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2026-09-11 07:06:49 -0300

    feat: insert header and footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,12 @@
 <script>
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
 </script>
 
+<Header />
 <main>
 </main>
+<Footer />
 
 <style>
 </style>

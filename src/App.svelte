41fb66310365054d3b152ed1c0e7b7f1401e3cdd
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2026-09-11 07:46:59 -0300

    feat: tab changing and resize height

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,32 @@
 <script>
   import Header from "./components/Header.svelte";
+  import Tabs from "./components/Tabs.svelte";
   import Footer from "./components/Footer.svelte";
+
+  // tabs
+  const existingTabs = ['Current Polls', 'Add New Poll'];
+  let currentTab = 'Current Polls';
+  const handleChangeTab = (e) => currentTab = e.detail;
+
+
 </script>
 
 <Header />
-<main>
+<main class="main-content">
+  <Tabs 
+    existingTabs={existingTabs} 
+    currentTabName={currentTab}
+    on:changeCurrentTab={handleChangeTab} 
+  />
+
+  <!-- New Poll Form 
+  -->
+  
 </main>
 <Footer />
 
 <style>
+  .main-content {
+    height: 75dvh;
+  }
 </style>

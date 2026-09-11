70818a6e7d355fff117e7ab8f6d0200d4d1120ae
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2026-09-11 07:06:27 -0300

    feat: create footer

---
+++ src/components/Header.svelte
@@ -0,0 +1,31 @@
+<script></script>
+
+<header class="app-header">
+  <h1 class="main-title">
+    Pollstorm
+  </h1>
+
+  <i class="pollstorm-icon bi bi-tornado"></i>
+</header>
+
+<style>
+  .app-header {
+    background-color: #000;
+    color: #fff;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 25dvh;
+  }
+
+  .main-title {
+    font-size: 2.2rem;
+    font-weight: 100;
+    letter-spacing: 1rem;
+  }
+
+  .pollstorm-icon {
+    font-size: 3rem;
+  }
+</style>

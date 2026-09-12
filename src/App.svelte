82aede2fb1b773d35b9387a7d07e5ea1200a6cff
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2026-09-12 05:44:01 -0300

    refactor: different components for each tab, with their own responsibility

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,9 @@
 <script>
+  // components
   import Header from "./components/Header.svelte";
   import Tabs from "./components/Tabs.svelte";
+  import NewPollForm from "./components/NewPollForm.svelte";
+  import PollsContainer from "./components/PollsContainer.svelte";
   import Footer from "./components/Footer.svelte";
 
   // tabs
@@ -8,25 +11,88 @@
   let currentTab = 'Current Polls';
   const handleChangeTab = (e) => currentTab = e.detail;
 
-
+  // polls
+  let activePolls = [
+    {
+      'id': 'ajsdhjkasdkhjsakhjgasj',
+      'question': 'Test 1',
+      'answer_a': {
+        'label': 'answerA',
+        'votes': 0
+      },
+      'answer_b': {
+        'label': 'answerB',
+        'votes': 0
+      }
+    },
+    {
+      'id': 'awkeuaweihguus',
+      'question': 'Test 2',
+      'answer_a': {
+        'label': 'answerA',
+        'votes': 0
+      },
+      'answer_b': {
+        'label': 'answerB',
+        'votes': 0
+      }
+    },
+    {
+      'id': 'ajsdhjkasdkhjssdakhjgasj',
+      'question': 'Test 3',
+      'answer_a': {
+        'label': 'answerA',
+        'votes': 0
+      },
+      'answer_b': {
+        'label': 'answerB',
+        'votes': 0
+      }
+    },
+    {
+      'id': 'awkeuaweihguuws',
+      'question': 'Test 4',
+      'answer_a': {
+        'label': 'answerA',
+        'votes': 0
+      },
+      'answer_b': {
+        'label': 'answerB',
+        'votes': 0
+      }
+    }
+  ];
 </script>
 
 <Header />
 <main class="main-content">
+  <!-- Tabs
+  -->
   <Tabs 
     existingTabs={existingTabs} 
     currentTabName={currentTab}
     on:changeCurrentTab={handleChangeTab} 
   />
 
+  <!-- Current Polls
+  -->
+  {#if currentTab === 'Current Polls'}
+    <PollsContainer polls={activePolls} />
+  {/if}
+
   <!-- New Poll Form 
   -->
-  
+  {#if currentTab === 'Add New Poll'}
+    <NewPollForm
+      on:changeTab={handleChangeTab} 
+      bind:polls={activePolls} 
+    />
+  {/if}
 </main>
 <Footer />
 
 <style>
   .main-content {
-    height: 75dvh;
+    height: 80dvh;
   }
 </style>

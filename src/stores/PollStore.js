536ef09c50f53730aa7aa80da8ba09ed7f44196e
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2026-09-13 09:13:31 -0300

    cleaning store

--- src/stores/PollStore.js
+++ src/stores/PollStore.js
@@ -1,18 +1,5 @@
 import { writable } from 'svelte/store';
 
-const PollStore = writable([
-  {
-    'id': `89132489238`,
-      'question': `a or b?`,
-      'answer_a': {
-        'label': `a is better`,
-        'votes': 0
-      },
-      'answer_b': {
-        'label': `b is better`,
-        'votes': 0
-      }
-  }
-]);
+const PollStore = writable([]);
 
 export default PollStore;

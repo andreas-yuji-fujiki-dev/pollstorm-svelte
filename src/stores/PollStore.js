d1166126ad2b95adc841f2804a91f504d1a41ed7
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2026-09-14 06:01:59 -0300

    feat: saving polls into localstorage

--- src/stores/PollStore.js
+++ src/stores/PollStore.js
@@ -1,5 +1,43 @@
 import { writable } from 'svelte/store';
 
-const PollStore = writable([]);
+const STORAGE_KEY = 'polls';
 
-export default PollStore;
+class PollStore {
+  constructor() {
+    const savedPolls = localStorage.getItem(STORAGE_KEY);
+
+    const initialData = savedPolls
+      ? JSON.parse(savedPolls)
+      : [];
+
+    this.store = writable(initialData);
+  }
+
+  subscribe(...args) {
+    return this.store.subscribe(...args);
+  }
+
+  set(newData) {
+    this.store.set(newData);
+    this.save(newData);
+  }
+
+  update(callback) {
+    this.store.update(currentData => {
+      const newData = callback(currentData);
+
+      this.save(newData);
+
+      return newData;
+    });
+  }
+
+  save(data) {
+    localStorage.setItem(
+      STORAGE_KEY,
+      JSON.stringify(data)
+    );
+  }
+}
+
+export default new PollStore();
